cf37262516506398c178dbec90d913ea0b6ccf00
Vigneshwaran S <vicky09989@gmail.com>
2023-09-08 13:19:32 +0530

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://0e1b-2401-4900-4df0-3977-c8a9-bb-31cf-d1df.ngrok-free.app/",
+    baseURL: "https://d6ce-2401-4900-4df0-3977-c8a9-bb-31cf-d1df.ngrok-free.app/",
     headers: {"ngrok-skip-browser-warning":"true"}
 });
 
